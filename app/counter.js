e13e40b73e06a1501468fe5e7f547cff03f9950c
Bpatalano <ben.patalano@gmail.com>
2017-01-29 19:45:09 -0800

    Logic for counter built out

---
+++ app/counter.js
@@ -0,0 +1,49 @@
+let $day = $('#day');
+let $hour = $('#hour');
+let $minute = $('#minute');
+let $second = $('#second');
+let $times = $('.count-box > input');
+let targetTime
+let editing = false;
+$times.on('blur', () => {
+  editing = false;
+  setTargetTime($day.val(), $hour.val(), $minute.val(), $second.val());
+});
+$times.on('focus', () => {
+  editing = true;
+});
+
+let setTargetTime = (day, hour, minute, second) => {
+  day = day * 86400000;
+  hour = hour * 3600000;
+  minute = minute * 60000;
+  second = second * 1000;
+  let now = Date.now();
+  timeToAdd = day + hour + minute + second;
+  targetTime = new Date(now + timeToAdd);
+}
+
+let changeTime = () => {
+  if (!editing) {
+    let newTime = targetTime - Date.now();
+    let day = Math.floor(newTime/86400000);
+    newTime = newTime % 86400000;
+    let hour = Math.floor(newTime/3600000);
+    newTime = newTime % 3600000;
+    let minute = Math.floor(newTime/60000);
+    newTime = newTime % 60000;
+    let second = Math.floor(newTime/1000);
+    if (!isNaN(newTime)) {
+      updateClock(day, hour, minute, second);
+    }
+  }
+}
+
+let updateClock = (day = 0, hour = 0, minute = 0, second = 0) => {
+  $day.val(day);
+  $hour.val(hour);
+  $minute.val(minute);
+  $second.val(second);
+}
+
+setInterval(changeTime, 1000);

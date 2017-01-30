9f245dc81cc1b19552a1687ed4e54abbc7b6d9bb
Bpatalano <ben.patalano@gmail.com>
2017-01-29 20:19:57 -0800

    Fixed for Heroku hosting

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 let express = require('express');
 let path = require('path');
 
+const PORT = process.env.PORT || 1337;
+
 let app = express();
 
 app.use('/', express.static(path.join(__dirname, './app')));
 
+app.get('*', function (req, res){
+    res.sendFile(path.resolve(__dirname, './app', 'index.html'));
+});
 
-var server = app.listen(1337, function(){
-  console.log('Time is ticking away on port 1337');
+var server = app.listen(PORT, function(){
+  console.log('Time is ticking away on port: ', PORT);
 });
